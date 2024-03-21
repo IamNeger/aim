d7e0035c62f129b3dad89ccf7accc3e3cb47ff7a
IamNeger <144994502+IamNeger@users.noreply.github.com>
2024-03-21 10:53:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,100 @@
-let tablo=document.getElementById("tablo")
-let score=0
-let timer=document.getElementById("timer")
-let time=60
+let canplay = false
+let reactionTimeout
+let miliInterval
+let bigdiv
+let reacshin = document.getElementById("reacshin")
+let tablo = document.getElementById("tablo")
+let score = 0
+let timer = document.getElementById("timer")
+let time = 60
 let speed = document.getElementById("speed")
 let modal = document.getElementById("modal")
+let ammo = 10
+reacshin.onclick = function (event) {
+    event.preventDefault()
+    modal.style.opacity = 0
+    bigdiv = document.createElement("div")
+    document.body.appendChild(bigdiv)
+    bigdiv.classList.add("bigdiv")
+    reaction()
+    bigdiv.onclick = function (event) {
+        ammo = ammo - 1
+        if (ammo == 0) {
+            modal.style.opacity = 1
+            bigdiv.remove()
+            canplay=false
+            
+        }
+        event.preventDefault()
+        bigdiv.style.backgroundColor = "white"
+        //стопит время
+        clearInterval(miliInterval)
+        if (canplay) {
+            canplay = false
+            reaction()
+        }
+    }
+}
+function reaction() {
+    reactionTimeout = setTimeout(() => {
+        canplay = true
+        bigdiv.style.backgroundColor = "lime"
+        time = 0
+        //интервал запускает мили секунды
+        miliInterval = setInterval(() => {
+            //добовляетс я время
+            time++
+            timer.innerHTML = "время: " + time
+        }, 1)
+        if (time > 0) {
+        }
+        //рандомная задержка
+    }, Math.random() * 4000 + 1000);
+}
 speed.onclick = function (event) {
     event.preventDefault()
     modal.style.opacity = 0
+    modal.style.pointerEvents = "none"
+    time = 60
+    score = 0
+    tablo.innerHTML = "счет: " + score
     createCircle()
-    let timeInterval=setInterval(() => {
+    let timeInterval = setInterval(() => {
         console.log(1);
-        time=time-1
-        if (time<1) {
-        clearInterval(timeInterval)
-        modal.style.opacity=1    
+        time = time - 10
+        if (time < 1) {
+            clearInterval(timeInterval)
+            modal.style.opacity = 1
+            modal.style.pointerEvents = "auto"
         }
-        timer.innerHTML="время: "+time
+        timer.innerHTML = "время: " + time
     }, 1000);
 }
 function createCircle() {
-    // делает елемент дива
+    // делает елемент дива(создают круг)
     let circle = document.createElement("div")
     // добавляет диву класс серкл 
     circle.classList.add("circle")
     // добовлял серкл в боди
     document.body.appendChild(circle)
     // нужно для того чтобы кружочки появлялись на рандомном расстоянии
-    circle.style.left = Math.random() *(window.innerWidth-50)  + "px"
-    circle.style.top = Math.random() * (window.innerHeight-50)+ "px"
+    circle.style.left = Math.random() * (window.innerWidth - 50) + "px"
+    circle.style.top = Math.random() * (window.innerHeight - 50) + "px"
     circle.onclick = function (event) {
         circle.remove()
-        if (time>0) {
-        createCircle()
+        if (time > 0) {
+            createCircle()
         }
         clearTimeout(circleTimeout)
-        score=score+1
-        tablo.innerHTML="счет: "+score
+        //+ одно очко
+        score = score + 1
+        tablo.innerHTML = "счет: " + score
     }
-    let circleTimeout=setTimeout(() => {
+    let circleTimeout = setTimeout(() => {
         circle.remove()
-        if (time>0) {
-        createCircle()    
+        if (time > 0) {
+            //создает кружочки
+            createCircle()
         }
     }, 2000);
 }
